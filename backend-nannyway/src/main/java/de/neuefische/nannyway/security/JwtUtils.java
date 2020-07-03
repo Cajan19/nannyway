f46ea064c379c8f73a71bc40b85738886c2e4c0b
Carolin Janshen <caro.janshen@gmail.com>
2020-07-03 17:24:53 +0200

    Add JwtUtils

---
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/security/JwtUtils.java
@@ -0,0 +1,24 @@
+package de.neuefische.nannyway.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtils {
+
+    private final String secret = "top-secret";
+
+    public String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(Duration.ofHours(4))))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
