737a3336ee4b2b8b44f227c32e33c45233a2e269
Carolin Janshen <caro.janshen@gmail.com>
2020-07-03 11:22:58 +0200

    Init project structure backend/frontend

---
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/NannywayApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.nannyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NannywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NannywayApplication.class, args);
+    }
+
+}
