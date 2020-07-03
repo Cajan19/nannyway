2aaa662659d6dc4c229f4d1bcf1b42e9335815b7
Carolin Janshen <caro.janshen@gmail.com>
2020-07-03 17:24:18 +0200

    Add LoginData model

---
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/model/LoginData.java
@@ -0,0 +1,15 @@
+package de.neuefische.nannyway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginData {
+
+    private String username;
+    private String password;
+
+}
