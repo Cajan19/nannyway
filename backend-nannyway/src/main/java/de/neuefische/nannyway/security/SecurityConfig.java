30e5ddb305c53e575265591e4d31bcc5a7ade939
Carolin Janshen <caro.janshen@gmail.com>
2020-07-03 17:25:23 +0200

    Update SecurityConfig

--- backend-nannyway/src/main/java/de/neuefische/nannyway/security/SecurityConfig.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/security/SecurityConfig.java
@@ -2,12 +2,13 @@ package de.neuefische.nannyway.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -32,10 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").authenticated()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**").authenticated()
+//                .antMatchers("/**").permitAll()
                 .and()
-                .formLogin();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
 
