2d81c6bb6c2c48c1f8c7065237a2f89a3ba0a4f5
Carolin Janshen <caro.janshen@gmail.com>
2020-07-05 18:38:26 +0200

    Add Navigation

--- frontend-nannyway/src/App.js
+++ frontend-nannyway/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-
+import React, {useContext, useEffect} from 'react';
 import './App.css';
+import LoginPage from "./pages/LoginPage";
+import UserContextProvider, {LOGIN_SUCCESS} from "./context/UserContextProvider";
+import Route from "react-router-dom/es/Route";
+import Switch from "@material-ui/core/Switch";
+import BrowserRouter from "react-router-dom/modules/BrowserRouter";
+import {UserDispatchContext} from "./context/UserContext";
+import {getDecodedJWTToken, isJWTTokenValid} from "./utils/jwt-utils";
+
+function Navigation() {
+    const dispatch = useContext(UserDispatchContext);
+
+    useEffect(() => {
+        if(isJWTTokenValid()) {
+            dispatch({type: LOGIN_SUCCESS, payload: getDecodedJWTToken()})
+        }
+    }, [dispatch])
+
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/login" exact>
+                <LoginPage/>
+            </Route>
+        </Switch>
+    </BrowserRouter>;
+}
+
 
 function App() {
   return (
     <div className="App">
-
+        <UserContextProvider>
+            <Navigation/>
+        </UserContextProvider>
     </div>
   );
 }
