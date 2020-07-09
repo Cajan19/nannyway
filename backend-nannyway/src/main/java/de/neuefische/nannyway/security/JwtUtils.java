73a44d64ac017361e47a71708d891fedd2d3ecc8
Carolin Janshen <caro.janshen@gmail.com>
2020-07-09 15:04:03 +0200

    Update jwt utils

--- backend-nannyway/src/main/java/de/neuefische/nannyway/security/JwtUtils.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/security/JwtUtils.java
@@ -1,6 +1,8 @@
 package de.neuefische.nannyway.security;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -11,7 +13,8 @@ import java.util.Map;
 @Service
 public class JwtUtils {
 
-    private final String secret = "top-secret";
+    @Value("${auth.jwt.secret}")
+    private String secret;
 
     public String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
@@ -21,5 +24,24 @@ public class JwtUtils {
                 .setExpiration(Date.from(Instant.now().plus(Duration.ofHours(4))))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration().before(new Date());
+    }
+
+    public String extractUserName(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    public Boolean validateToken(String token, String username) {
+        final String extractUserName = extractUserName(token);
+        return (extractUserName.equals(username) && !isTokenExpired(token));
     }
 }
