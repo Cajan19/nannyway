aed265acccd415ccddac937ecf40222ee3ea144f
Carolin Janshen <caro.janshen@gmail.com>
2020-07-11 11:04:38 +0200

    Clean up file structure/adjust paths

--- frontend-nannyway/src/App.js
+++ frontend-nannyway/src/App.js
@@ -1,34 +1,7 @@
-import React, {useContext, useEffect} from "react";
-import {UserDispatchContext} from "./context/user/UserContext";
-import {getDecodedJWTToken, isJWTTokenValid} from "./utils/jwt-utils";
-import {BrowserRouter} from "react-router-dom";
-import {Route, Switch} from "react-router";
-import PrivateRoute from "./pages/PrivateRoute";
-import ChildInCareOverview from "./pages/ChildInCareOverview";
-import LoginPage from "./pages/LoginPage";
-import UserContextProvider, {LOGIN_SUCCESS} from "./context/user/UserContextProvider";
-import ProminentAppBar from "./components/ProminentAppBar";
+import React from "react";
+import UserContextProvider from "./context/user/UserContextProvider";
+import Navigation from "./components/navigation/Navigation";
 
-
-function Navigation() {
-    const dispatch = useContext(UserDispatchContext);
-
-    useEffect(() => {
-        if (isJWTTokenValid()) {
-            dispatch({type: LOGIN_SUCCESS, payload: getDecodedJWTToken()})
-        }
-    }, [dispatch])
-
-    return <BrowserRouter>
-        <ProminentAppBar />
-        <Switch>
-            <PrivateRoute path="/" component={ChildInCareOverview} exact/>
-            <Route path="/login" exact>
-                <LoginPage/>
-            </Route>
-        </Switch>
-    </BrowserRouter>;
-}
 
 function App() {
     return (
