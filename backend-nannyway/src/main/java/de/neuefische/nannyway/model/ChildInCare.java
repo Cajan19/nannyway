4d78ddb69c9b2a98edca2e1cfb820cccd2380264
Carolin Janshen <caro.janshen@gmail.com>
2020-07-14 12:37:32 +0200

    Add MongoDB mapping

--- backend-nannyway/src/main/java/de/neuefische/nannyway/model/ChildInCare.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/model/ChildInCare.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "childInCare")
 public class ChildInCare {
     @Id
     private String id;
