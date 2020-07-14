3bdd206954294a3537e01506ccfd79361b96d8ba
Carolin Janshen <caro.janshen@gmail.com>
2020-07-14 12:34:58 +0200

    Add NotEmpty validation for first name field

--- backend-nannyway/src/main/java/de/neuefische/nannyway/model/AddChildInCareDto.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/model/AddChildInCareDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class AddChildInCareDto {
 
+    @NotEmpty(message = "Bitte gib einen Vornamen ein!")
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
