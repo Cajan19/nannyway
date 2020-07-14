d38337669d6e7bed44d2598829ae59c58f0214ea
Carolin Janshen <caro.janshen@gmail.com>
2020-07-14 18:29:02 +0200

    Add mui ThemeProvider

--- frontend-nannyway/src/index.js
+++ frontend-nannyway/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {MuiThemeProvider} from "@material-ui/core";
+import myTheme from "./styling/muiTheme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <MuiThemeProvider theme={myTheme}>
+        <App/>
+        </MuiThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
