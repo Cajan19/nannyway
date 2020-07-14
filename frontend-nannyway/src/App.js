03dffd78df0e3d58ad63ab2d6d868a65853fe841
Carolin Janshen <caro.janshen@gmail.com>
2020-07-14 18:30:10 +0200

    Add CssBaseline

--- frontend-nannyway/src/App.js
+++ frontend-nannyway/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import UserContextProvider from "./context/user/UserContextProvider";
 import Navigation from "./components/navigation/Navigation";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 
 function App() {
     return (
         <div className="App">
             <UserContextProvider>
+                <CssBaseline>
                 <Navigation/>
+                </CssBaseline>
             </UserContextProvider>
         </div>
     );
