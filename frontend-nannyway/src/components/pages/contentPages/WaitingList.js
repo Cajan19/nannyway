80c9f459485cb13fcfb9180c86d17fbd1a87fbe4
Carolin Janshen <caro.janshen@gmail.com>
2020-07-15 10:44:01 +0200

    Create basic pages for waitinglist

---
+++ frontend-nannyway/src/components/pages/contentPages/WaitingList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import ProminentAppBar from "../../header/ProminentAppBar";
+
+export default function WaitingList() {
+    return (
+        <>
+            <ProminentAppBar/>
+            <Typography>
+                WaitingList
+            </Typography>
+        </>
+    )
+}
