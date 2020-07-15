1b46452e35984fd34a9bbf9a77b2e33e9fcdffd5
Carolin Janshen <caro.janshen@gmail.com>
2020-07-15 16:49:59 +0200

    Add logout functionality

--- frontend-nannyway/src/components/footer/LogoutButton.js
+++ frontend-nannyway/src/components/footer/LogoutButton.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 import Button from "@material-ui/core/Button";
+import {UserDispatchContext} from "../../context/user/UserContext";
+import {LOGOUT} from "../../context/user/UserContextProvider";
 
 export default function LogoutButton() {
+    const dispatch = useContext(UserDispatchContext);
+
+    function logout() {
+        dispatch({type: LOGOUT})
+    }
 
     return (
-        <Button color={"primary"} variant={"contained"} size={"small"}>Logout</Button>
+        <Button onClick={logout} color={"primary"} variant={"contained"} size={"small"}>
+            Logout
+        </Button>
     );
-
 }
