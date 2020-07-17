fbfeaf1f2104ec0006342c30b537cba833864b04
Carolin Janshen <caro.janshen@gmail.com>
2020-07-17 15:15:10 +0200

    Add function to add a kid

--- frontend-nannyway/src/context/childInCare/childInCareActions.js
+++ frontend-nannyway/src/context/childInCare/childInCareActions.js
@@ -1,8 +1,12 @@
-import {fetchAllKids} from "../../utils/childInCareUtils";
+import {fetchAllKids, putKid} from "../../utils/childInCareUtils";
 
 export const FETCH_KIDS = `FETCH_KIDS`;
 export const FETCH_KIDS_SUCCESS = `FETCH_KIDS_SUCCESS`;
 export const FETCH_KIDS_FAIL = `FETCH_KIDS_FAIL`;
+export const ADD_KID = `ADD_KID`;
+export const ADD_KID_SUCCESS = `ADD_KID_SUCCESS`;
+export const ADD_KID_FAIL = `ADD_KID_FAIL`;
+
 
 export async function fetchKids(dispatch) {
     dispatch({type: FETCH_KIDS});
@@ -11,5 +15,15 @@ export async function fetchKids(dispatch) {
         dispatch({type: FETCH_KIDS_SUCCESS, payload: kids});
     } catch (error) {
         dispatch({type: FETCH_KIDS_FAIL, payload: error});
+    }
+}
+
+export async function addKid(dispatch, firstName, lastName, birthDate) {
+    dispatch({ type: ADD_KID});
+    try {
+        const kid = await putKid(firstName, lastName, birthDate);
+        dispatch({ type: ADD_KID_SUCCESS, payload: kid});
+    } catch (error) {
+        dispatch({ type: ADD_KID_FAIL, payload: error});
     }
 }
