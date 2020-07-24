411731dbbc0792fb4cdaeebe181f8fd3d38da273
Carolin Janshen <caro.janshen@gmail.com>
2020-07-24 18:00:06 +0200

    Add post/delete functions

--- frontend-nannyway/src/utils/childOnWaitingListUtils.js
+++ frontend-nannyway/src/utils/childOnWaitingListUtils.js
@@ -14,3 +14,46 @@ export async function fetchAllWaitingKids() {
     }
     return await response.json();
 }
+
+export function postWaitingKid(familyName, firstName, birthDate, phoneNumber, email, getToKnowDate,
+                               startDateOfCare, hoursInCarePerWeek, prediction, approval, infoText) {
+    const token = getJWTToken();
+    const childOnWaitingListData = {
+        familyName: familyName,
+        firstName: firstName,
+        birthDate: birthDate,
+        phoneNumber: phoneNumber,
+        email: email,
+        getToKnowDate: getToKnowDate,
+        startDateOfCare: startDateOfCare,
+        hoursInCarePerWeek: hoursInCarePerWeek,
+        prediction: prediction,
+        approval: approval,
+        infoText: infoText,
+    }
+    return fetch("api/waitinglist", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(childOnWaitingListData),
+    }).then((response) => {
+        if (response.status !== 200) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+    });
+}
+
+export function removeWaitingKid(id) {
+    const token = getJWTToken();
+
+    return fetch(`/api/waitinglist/${id}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+}
+
