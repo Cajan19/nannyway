abc71a7e75d7342df53894126bdc1e3bb12411e7
Carolin Janshen <caro.janshen@gmail.com>
2020-07-24 18:00:30 +0200

    Add switch cases

--- frontend-nannyway/src/context/childOnWaitingList/childOnWaitingListReducer.js
+++ frontend-nannyway/src/context/childOnWaitingList/childOnWaitingListReducer.js
@@ -1,4 +1,9 @@
-import {FETCH_WAITINGKIDS, FETCH_WAITINGKIDS_FAIL, FETCH_WAITINGKIDS_SUCCESS} from "./childOnWaitingListActions";
+import {
+    ADD_WAITINGKID, ADD_WAITINGKID_FAIL, ADD_WAITINGKID_SUCCESS, DELETE_WAITINGKID_SUCCESS,
+    FETCH_WAITINGKIDS,
+    FETCH_WAITINGKIDS_FAIL,
+    FETCH_WAITINGKIDS_SUCCESS
+} from "./childOnWaitingListActions";
 
 export default function childOnWaitingListReducer(state, action) {
     switch (action.type) {
@@ -8,6 +13,18 @@ export default function childOnWaitingListReducer(state, action) {
             return {...state, fetchStatus: `SUCCESS`, waitingKids: action.payload};
         case FETCH_WAITINGKIDS_FAIL:
             return {...state, fetchStatus: `FAILED`};
+        case ADD_WAITINGKID:
+            return {...state, addStatus: `PENDING`};
+        case ADD_WAITINGKID_SUCCESS:
+            return {...state, addStatus: `SUCCESS`, waitingKids: [...state.waitingKids, action.payload]};
+        case ADD_WAITINGKID_FAIL:
+            return {...state, addStatus: `FAILED`};
+        case DELETE_WAITINGKID_SUCCESS:
+            return {
+                ...state, waitingKids: state.waitingKids.filter((waitingKid) => {
+                    return waitingKid.id !== action.payload;
+                }),
+            };
         default:
             return state;
     }
