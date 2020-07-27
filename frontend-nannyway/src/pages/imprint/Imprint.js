437761ca2096f28cad566e3379cde123b611362f
Carolin Janshen <caro.janshen@gmail.com>
2020-07-27 12:36:41 +0200

    Add imprint info

--- frontend-nannyway/src/pages/imprint/Imprint.js
+++ frontend-nannyway/src/pages/imprint/Imprint.js
@@ -10,11 +10,16 @@ import FooterImprint from "../../components/footer/FooterImprint";
 const useStyles = makeStyles((theme) => ({
     heading: {
         fontFamily: "Finger Paint",
-        color: myTheme.palette.primary.dark,
+        color: myTheme.palette.info.light,
+    },
+    headingImprint: {
+        fontFamily: "Open Sans",
+        fontWeight: "bold",
+        color: myTheme.palette.info.main,
     },
     basictypo: {
         fontFamily: "Open Sans",
-        color: myTheme.palette.info.light,
+        color: myTheme.palette.info.main,
     },
     image: {
         backgroundImage: 'url("https://images.unsplash.com/photo-1530036067142-31e9b88da9b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80")',
@@ -26,14 +31,14 @@ const useStyles = makeStyles((theme) => ({
     paperTop: {
         padding: theme.spacing(2),
         textAlign: 'center',
-        backgroundColor: myTheme.palette.secondary.main,
+        backgroundColor: myTheme.palette.primary.main,
         fontFamily: "Open Sans",
         marginBottom: "20px",
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
-        backgroundColor: myTheme.palette.secondary.light,
+        backgroundColor: myTheme.palette.info.dark,
         marginBottom: 10,
     },
 }));
@@ -43,7 +48,7 @@ export default function Imprint() {
 
     return (
         <div>
-            <NannyAppBar colorStyle={"secondary"}/>
+            <NannyAppBar colorStyle={"primary"}/>
             <main>
                 <div className={classes.image}>
                     <Paper className={classes.paperTop}>
@@ -57,18 +62,62 @@ export default function Imprint() {
                           alignItems="center">
                         <Grid item xs={10}>
                             <Paper className={classes.paper}>
-                                <Typography className={classes.basictypo}>
-                                    Dinge und Sachen!!!
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                <Typography align={"left"} className={classes.headingImprint}>
+                                    Verantwortlich für den Inhalt dieser Seiten:
+                                </Typography>
+                            <Typography paragraph />
+                            <Typography align={"left"} className={classes.basictypo}>
+                                Carolin Janshen
+                            </Typography>
+                            <Typography align={"left"} className={classes.basictypo}>
+                                Ölbergstraße 1
+                            </Typography>
+                            <Typography align={"left"} className={classes.basictypo}>
+                                93173 Wenzenbach
+                            </Typography>
+                                <Typography paragraph/>
+                                <Typography align={"left"} className={classes.headingImprint}>
+                                    Kontakt:
+                                </Typography>
+                            <Typography align={"left"} className={classes.basictypo}>
+                                caro.janshen@gmail.com
+                            </Typography>
+                            <Typography paragraph/>
+                            <Typography align={"left"} className={classes.headingImprint}>
+                                Haftung für Inhalte:
+                            </Typography>
+                            <Typography align={"left"} className={classes.basictypo}>
+                                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+                            </Typography>
+                                <Typography paragraph/>
+                                <Typography align={"left"} className={classes.headingImprint}>
+                                    Haftung für Links:
+                                </Typography>
+                                <Typography align={"left"} className={classes.basictypo}>
+                                    Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+                                </Typography>
+                                <Typography paragraph/>
+                                <Typography align={"left"} className={classes.headingImprint}>
+                                    Urheberrecht:
+                                </Typography>
+                                <Typography align={"left"} className={classes.basictypo}>
+                                    Die durch den Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+                                    Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+                                </Typography>
+                                <Typography paragraph/>
+                                <Typography align={"left"} className={classes.headingImprint}>
+                                    Quellenangaben:
+                                </Typography>
+                                <Typography align={"left"} className={classes.basictypo}>
+
                                 </Typography>
                             </Paper>
+
                         </Grid>
                     </Grid>
                 </div>
             </main>
-            <FooterImprint colorStyle={"secondary"}/>
+            <FooterImprint colorStyle={"primary"}/>
         </div>
 )
 }
