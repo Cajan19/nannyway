16a680bc628509702e852c12f05c7cb2df8876e5
Carolin Janshen <caro.janshen@gmail.com>
2020-07-28 10:36:20 +0200

    Add Optional

--- backend-nannyway/src/main/java/de/neuefische/nannyway/database/UserDb.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/database/UserDb.java
@@ -3,5 +3,8 @@ package de.neuefische.nannyway.database;
 import de.neuefische.nannyway.model.NannywayUser;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface UserDb extends PagingAndSortingRepository<NannywayUser, String>{
+    Optional<NannywayUser>findByUsername(String username);
 }
