eea8b88e31fb00f9df61a6194a0c1f99cd896544
Carolin Janshen <caro.janshen@gmail.com>
2020-07-29 19:45:23 +0200

    Add userUtils & log error

--- backend-nannyway/src/main/java/de/neuefische/nannyway/controller/LoginController.java
+++ backend-nannyway/src/main/java/de/neuefische/nannyway/controller/LoginController.java
@@ -1,6 +1,9 @@
 package de.neuefische.nannyway.controller;
+
 import de.neuefische.nannyway.model.LoginData;
 import de.neuefische.nannyway.security.JwtUtils;
+import de.neuefische.nannyway.utils.UserUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,24 +15,30 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 
+@Slf4j
 @RestController
 @RequestMapping("auth/login")
 public class LoginController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final UserUtils userUtils;
 
-    public LoginController(AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+    public LoginController(AuthenticationManager authenticationManager, JwtUtils jwtUtils, UserUtils userUtils) {
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
+        this.userUtils = userUtils;
     }
 
     @PostMapping
     public String login(@RequestBody LoginData data) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(data.getUsername(), data.getPassword()));
-            return jwtUtils.createToken(new HashMap<>(), data.getUsername());
-        }catch (Exception e){
+            HashMap<String, Object> tokenUserData = userUtils.getUserTokenData(data.getUsername());
+
+            return jwtUtils.createToken(tokenUserData, data.getUsername());
+        } catch (Exception e) {
+            log.error("failed to login", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid credentials");
         }
     }
