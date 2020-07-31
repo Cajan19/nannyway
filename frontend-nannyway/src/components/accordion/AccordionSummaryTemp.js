e1a6491897d5e8e3651c7e16c50593ed8d4a2dfb
Carolin Janshen <caro.janshen@gmail.com>
2020-07-31 10:26:45 +0200

    Create AccordionSummary templates

---
+++ frontend-nannyway/src/components/accordion/AccordionSummaryTemp.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+
+export default function AccordionSummaryTemp({expandClass, typoClass, typoValue}) {
+    return (
+        <AccordionSummary
+            expandIcon={<ExpandMoreIcon className={expandClass}/>}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+        >
+            <Grid spacing={6}
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+            >
+                <Typography className={typoClass}>{typoValue}</Typography>
+            </Grid>
+        </AccordionSummary>
+    )
+}
