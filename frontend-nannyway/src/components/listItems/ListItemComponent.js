adc8b9ccddd75db47162e771d01b346df488677f
Carolin Janshen <caro.janshen@gmail.com>
2020-08-03 11:33:48 +0200

    Add tooltip

--- frontend-nannyway/src/components/listItems/ListItemComponent.js
+++ frontend-nannyway/src/components/listItems/ListItemComponent.js
@@ -3,14 +3,23 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
 
-export default function ListItemComponent({itemIcon, typoClass, typoValue, iconColor}) {
+export default function ListItemComponent({itemIcon, typoClass, typoValue, iconColor, toolTip}) {
     return (
         <List>
             <ListItem>
+                <Tooltip
+                    title={toolTip}
+                    placement={"top-start"}
+                    arrow={true}
+                    disableFocusListener={true}
+                    TransitionComponent={Zoom}>
                 <ListItemIcon className={iconColor}>
                     {itemIcon}
                 </ListItemIcon>
+                </Tooltip>
                     <Typography className={typoClass} gutterBottom={false}>
                         {typoValue}
                     </Typography>
--- frontend-nannyway/src/components/listItems/ListItemComponentMultiline.js
+++ frontend-nannyway/src/components/listItems/ListItemComponentMultiline.js
@@ -4,14 +4,23 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import Grid from "@material-ui/core/Grid";
+import Zoom from "@material-ui/core/Zoom";
+import Tooltip from "@material-ui/core/Tooltip";
 
-export default function ListItemComponentMultiline({itemIcon, typoClass, typoValueOne, typoValueTwo, iconColor}) {
+export default function ListItemComponentMultiline({itemIcon, toolTip, typoClass, typoValueOne, typoValueTwo, iconColor}) {
     return (
         <List>
             <ListItem>
-                <ListItemIcon className={iconColor}>
-                    {itemIcon}
-                </ListItemIcon>
+                <Tooltip
+                    title={toolTip}
+                    placement={"top-start"}
+                    arrow={true}
+                    disableFocusListener={true}
+                    TransitionComponent={Zoom}>
+                    <ListItemIcon className={iconColor}>
+                        {itemIcon}
+                    </ListItemIcon>
+                </Tooltip>
                 <Grid>
                     <Typography className={typoClass} paragraph>
                         {typoValueOne}
